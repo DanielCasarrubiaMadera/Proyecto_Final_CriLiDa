962e0714a2f2a7e71281941198fb297d90f31267
ccausil <ccausilquintero70@correo.unicordoba.edu.co>
2021-05-20 20:26:07 -0500

    Bienvenida Cristian Causil
    Cambio realizado por el docente en formación Cristian Causill

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const port = 3000;
 
 app.get('/', (req, res) => { 
   res.send('Bienvenidos a nuestro buscador de videos educativos, que esta enfatizado en el área de Ciencias Naturales'); 
+});
+app.get('/', (req, res) => { 
+  res.send('En este buscador podras tener una experiencia unica al momento de buscar videos educativos en base al area de ciencias naturales'); 
 }) 
 
 app.listen(port, () => { 
