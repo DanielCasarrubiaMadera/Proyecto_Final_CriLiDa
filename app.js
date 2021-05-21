24f8533944d05dd8a8fa6bc0595a532986a032d5
LinaLópezAmador <Llopezamador34@correo.unicordoba.edu.co>
2021-05-20 20:36:53 -0500

    Bienvenida Lina López
    Cambio realizado por la estudiante Lina López

--- app.js
+++ app.js
@@ -3,11 +3,9 @@ const app = express();
 const port = 3000; 
 
 app.get('/', (req, res) => { 
-  res.send('Bienvenidos a nuestro buscador de videos educativos, que esta enfatizado en el área de Ciencias Naturales'); 
+  res.send('Bienvenidos todos, este es un espacio enfatizado en las ciencias naturales para que aprendamos a buscar videos educativos relacionados con el área. '); 
 });
-app.get('/', (req, res) => { 
-  res.send('En este buscador podras tener una experiencia unica al momento de buscar videos educativos en base al area de ciencias naturales'); 
-}) 
+
 
 app.listen(port, () => { 
   console.log(`Example app listening at http://localhost:${port}`); 
